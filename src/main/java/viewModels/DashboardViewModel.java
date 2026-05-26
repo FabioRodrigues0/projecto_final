eeeca0aeb55ccfd9fc56d0e3b20994343156cc98
Fábio Rodrigues <fabio.rod@outlook.pt>
2026-05-26 17:08:01 +0100

    format: fix format

--- src/main/java/viewModels/DashboardViewModel.java
+++ src/main/java/viewModels/DashboardViewModel.java
@@ -8,19 +8,20 @@ import java.util.List;
 import models.Expiracoes;
 
 public class DashboardViewModel extends BricksViewModel {
-    public final State<Integer> qntDocumentos = state(0);
-    public final State<Integer> qntVeiculos = state(0);
-    public final State<Integer> qntSubscricoes = state(0);
-    public final State<Integer> qntAlertas = state(0);
-    public final StateList<Expiracoes> listExpiracoes = stateList(List.of());
+  public final State<Integer> qntDocumentos = state(0);
+  public final State<Integer> qntVeiculos = state(0);
+  public final State<Integer> qntSubscricoes = state(0);
+  public final State<Integer> qntAlertas = state(0);
+  public final StateList<Expiracoes> listExpiracoes = stateList(List.of());
 
-    public void carregarExpiracoes() {
-        List<Expiracoes> lista = DB
-            .query()
+  public void carregarExpiracoes() {
+    List<Expiracoes> lista =
+        DB.query()
             .select("a.id", "d.nome", "a.genero", "a.idade", "a.media", "nd.nota")
             .from("alunos a")
             .execute(Expiracoes.class);
-        listExpiracoes.clear();
-        listExpiracoes.addAll(lista);
-    }
+
+    listExpiracoes.clear();
+    listExpiracoes.addAll(lista);
+  }
 }
