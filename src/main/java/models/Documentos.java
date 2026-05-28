79297b508f73c4782592379c7e831574cc49975c
Fabio Rodrigues <fabio.rod@outlook.pt>
2026-05-28 13:17:06 +0100

    documentos: faltava os contrutores no models, e get an set de id

--- src/main/java/models/Documentos.java
+++ src/main/java/models/Documentos.java
@@ -1,10 +1,33 @@
 package models;
 
 public abstract class Documentos {
+    public int id;
     public String titulo;
     public String notas;
 
+    public Documentos() {
+    }
+
+    public Documentos(int id, String titulo) {
+        this.id = id;
+        this.titulo = titulo;
+    }
+
+    public Documentos(int id, String titulo, String notas) {
+        this.id = id;
+        this.titulo = titulo;
+        this.notas = notas;
+    }
+
+    public abstract int getId();
+
+    public abstract void setId(int id);
+
     public abstract String getTitulo();
 
+    public abstract void setTitulo(String titulo);
+
     public abstract String getNotas();
+
+    public abstract void setNotas(String notas);
 }
